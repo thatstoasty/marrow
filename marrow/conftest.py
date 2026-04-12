a0a373eb917f19223342fc1337da47d2e51ba1ac
Mikhail Tavarez <miktavarez@gmail.com>
2026-04-12 14:33:09 -0500

    wip

--- marrow/conftest.py
+++ marrow/conftest.py
@@ -398,21 +398,21 @@ def pytest_sessionstart(session):
     if _python_excluded(config):
         return
 
-    print("building marrow_python/marrow_python/marrow.so ...", flush=True)
-    opt = "-O3" if config.getoption("--benchmark") else "-O1"
-    cmd = (
-        ["mojo", "build", opt, "-I", "."]
-        # ["pixi", "run", "python_build"]
-        + MojoRunner.asan_flags(config)
-        + ["marrow_python/lib.mojo", "--emit", "shared-lib", "-o", "marrow_python/marrow.so"]
-    )
-    result = subprocess.run(cmd, cwd=config.rootpath / "marrow_python", capture_output=True, text=True)
-    if result.returncode != 0:
-        pytest.exit(
-            f"Failed to build python/marrow.so:\n{result.stderr}",
-            returncode=1,
-        )
-    print("python/marrow.so built successfully", flush=True)
+    # print("building marrow_python/marrow_python/marrow.so ...", flush=True)
+    # opt = "-O3" if config.getoption("--benchmark") else "-O1"
+    # cmd = (
+    #     ["mojo", "build", opt, "-I", "."]
+    #     # ["pixi", "run", "python_build"]
+    #     + MojoRunner.asan_flags(config)
+    #     + ["marrow_python/lib.mojo", "--emit", "shared-lib", "-o", "marrow_python/marrow.so"]
+    # )
+    # result = subprocess.run(cmd, cwd=config.rootpath / "marrow_python", capture_output=True, text=True)
+    # if result.returncode != 0:
+    #     pytest.exit(
+    #         f"Failed to build python/marrow.so:\n{result.stderr}",
+    #         returncode=1,
+    #     )
+    # print("python/marrow.so built successfully", flush=True)
 
 
 def pytest_collection_modifyitems(config, items):
